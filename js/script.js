10faab29e9dc8a60ef95dd28346a5d6d11e108d5
Gabriel Garcia-Miro Jeri <ggmj@Gabriels-MacBook-Pro-2.local>
2023-11-17 09:42:32 -0500

    avances

--- js/script.js
+++ js/script.js
@@ -1,2 +1,31 @@
-import peliculas from './peliculas.js'
+import peliculas from './peliculas'
 
+const rutaRelative = "https://image.tmdb.org/t/p/w500";
+
+const generoAccion = 28;
+const generoThriller = 53;
+const generoAventura = 12;
+
+const accion = peliculas.filter(peli => peli.genre_ids.includes(28));
+
+const contenedor = document.createElement("div");
+contenedor.classList.add("genero");
+
+peliculasPorGenero.forEach(peli => {
+    const peliContenedor = document.createElement("div");
+    peliContenedor.classList.add("peli-contenedor");
+
+    const imagen = document.createElement("img");
+    imagen.src = rutaUrl + peli.poster_path;
+    imagen.alt = peli.title;
+
+    const titulo = document.createElement("h3");
+    titulo.textContent = peli.title;
+
+    peliContenedor.appendChild(imagen);
+    peliContenedor.appendChild(titulo);
+    contenedor.appendChild(peliContenedor)
+})
+
+const seccion = document.querySelector(".genero-container");
+seccion.appendChild(container);
